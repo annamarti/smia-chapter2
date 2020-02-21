fd19004639b45fb48a962abeeec321958d237fb3
annamarti <anna.martirosyan@mail.com>
2020-02-21 18:03:12 +0400

    Added passwordEncoder in specialroutes service

--- authentication-service/src/main/java/com/example/authentication/security/OAuth2Config.java
+++ authentication-service/src/main/java/com/example/authentication/security/OAuth2Config.java
@@ -2,17 +2,25 @@ package com.example.authentication.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 @Configuration
+@EnableAuthorizationServer
+@EnableResourceServer
 public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
+    @Qualifier ("authenticationManagerBean")
     private AuthenticationManager authenticationManager;
 
     @Qualifier("userDetailsServiceBean")
@@ -37,4 +45,11 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
                 .authenticationManager(authenticationManager)
                 .userDetailsService(userDetailsService);
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // To avoid IllegalArgumentException: There is no PasswordEncoder mapped for the id "null"
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
