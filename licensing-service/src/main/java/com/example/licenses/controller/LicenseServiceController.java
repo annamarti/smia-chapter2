c280981d0199ea05a33836c3790a78804a59a2f3
annamarti <anna.martirosyan@mail.com>
2020-02-25 17:04:42 +0400

    Removed some redundant dependencies

--- licensing-service/src/main/java/com/example/licenses/controller/LicenseServiceController.java
+++ licensing-service/src/main/java/com/example/licenses/controller/LicenseServiceController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/organizations/{organizationId}/licences")
+@RequestMapping("/v1/organizations/{organizationId}/licenses")
 public class LicenseServiceController {
 
     @Autowired
--- organization-service/src/main/java/com/example/organizationservice/OrganizationApp.java
+++ organization-service/src/main/java/com/example/organizationservice/OrganizationApp.java
@@ -3,9 +3,11 @@ package com.example.organizationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableResourceServer  //is used to tell your microservice it’s a protected resource
 public class OrganizationApp {
     public static void main(String[] args) {
         SpringApplication.run(OrganizationApp.class, args);
